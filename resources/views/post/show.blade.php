5f68ba977e9793ceadb4adda04dad3f3cba9cc69
makhovdmitrii <makhovdmitrii@gmail.com>
2023-02-13 17:36:58 +0300

    if blade syntax

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -11,4 +11,36 @@ Current timestamp: {{ time() }}
 @if ($isAuth)
     вы авторизованы
 @endif
+@if (!$isAuth)
+    вы не авторизованы
+@endif
+@if ($isAuth)
+    вы авторизованы
+@else
+    вы не авторизованы
+@endif
+        @if ($num > 0)
+            больше нуля
+        @elseif ($num === 0)
+            ноль
+        @else
+            меньше нуля
+        @endif
+
+    @if (!$isAuth)
+            вы еще не авторизованы
+    @endif
+    @unless ($isAuth)
+            вы еще не авторизованы
+    @endunless
+        @if (count($arr) >= 1)
+            в массиве есть записи
+        @else
+            в массиве нет записей
+        @endif
+        <ul>
+            @foreach ($arr as $elem)
+                <li>{{ $elem }}</li>
+            @endforeach
+        </ul>
 </x-layout>
