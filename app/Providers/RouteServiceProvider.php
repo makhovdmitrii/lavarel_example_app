0b1532b8257e38134ffcd65b80a76523a7ac22ac
makhovdmitrii <makhovdmitrii@gmail.com>
2023-02-13 16:15:42 +0300

    Routes examples

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,6 +36,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+        // https://code.mu/ru/php/framework/laravel/book/prime/routing/global-parameters-constraints/
+        Route::pattern('id', '[0-9]+');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//$users = User::all();
+//dd($users);
+//dump($users);
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts', function () {
+    return 'список постов';
+});
+Route::prefix('post')->group(function () {
+    Route::get('/{id}', function ($id) {
+        return 'пост ' . $id;
+    })->where('id', '[0-9]+');
+    Route::get('/page/{page?}', function ($page = 1) {
+        return 'страница номер ' . $page;
+    })->whereNumber('page');
+    Route::get('/{catId}/{postId}', function ($catId, $postId) {
+        return $catId . ' ' . $postId;
+    })->where('catId', '[a-z0-9_-]+')->where('postId', '[0-9]+');
+});
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return $id . ' ' . $name;
+})->whereAlpha('name')->whereAlphaNumeric('id');
+
