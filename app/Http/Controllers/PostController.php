0c303253d5d0af68536deed1aac136ecd6841dae
makhovdmitrii <makhovdmitrii@gmail.com>
2023-02-13 17:28:41 +0300

    some blade syntax

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -54,7 +54,7 @@ class PostController extends Controller
             5 => 'текст 5',
         ];
 
-        return view('post.show', ['post_text' => $posts[$id], 'title' => 'page title',]);
+        return view('post.show', ['post_text' => $posts[$id], 'title' => 'page title', 'arr' => [1, 2], 'isAuth' => true]);
     }
 
     /**
