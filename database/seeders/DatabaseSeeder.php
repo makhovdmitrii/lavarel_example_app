7f1973b4eba243d8570fec7fad844a8526972965
makhovdmitrii <makhovdmitrii@gmail.com>
2023-03-06 17:43:58 +0300

    php artisan migrate:refresh --step=2 php artisan db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,25 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('articles')->insert([
+            [
+                'name' => 'title 1',
+                'title'  => 'post-1',
+                'message'  => 'text text text 1',
+                'vote' => '0',
+            ],
+            [
+                'name' => 'title 2',
+                'title'  => 'post-2',
+                'message'  => 'text text text 2',
+                'vote' => '0',
+            ],
+            [
+                'name' => 'title 3',
+                'title'  => 'post-3',
+                'message'  => 'text text text 3',
+                'vote' => '0',
+            ],
+        ]);
     }
 }
