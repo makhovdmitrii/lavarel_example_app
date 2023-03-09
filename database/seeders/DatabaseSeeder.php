85baa2434d9f42716c32b2d9827e6c5937a081db
makhovdmitrii <makhovdmitrii@gmail.com>
2023-03-09 14:26:18 +0300

    reverting to previous single file seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,9 +23,30 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            ArticlesSeeder::class,
-            UsersSeeder::class,
+        DB::table('articles')->insert([
+            [
+                'name' => 'title 1',
+                'title'  => 'post-1',
+                'message'  => 'text text text 1' . Str::random(50),
+                'vote' => '0',
+            ],
+            [
+                'name' => 'title 2',
+                'title'  => 'post-2',
+                'message'  => 'text text text 2' . Str::random(25),
+                'vote' => '0',
+            ],
+            [
+                'name' => 'title 3',
+                'title'  => 'post-3',
+                'message'  => 'text text text 3' . Str::random(10),
+                'vote' => '0',
+            ],
+        ]);
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('12345'),
         ]);
     }
 }
