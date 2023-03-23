1599e7a4ecf3762e2cd1fec4b1c30670e0b6a627
makhovdmitrii <makhovdmitrii@gmail.com>
2023-03-23 16:24:52 +0300

    adding used commands in workflow

---
+++ commands.sh
@@ -0,0 +1,42 @@
+composer create-project laravel/laravel laravel_example_app
+# TODO Filter unused
+php artisan preset none
+/usr/bin/php7.4 artisan preset none
+php artisan preset none
+php artisan preset none
+php artisan make:model Game -mrc
+php artisan preset none
+php artisan serve
+php artisan route:list
+php artisan serve
+php artisan list
+php artisan route:list
+php artisan about
+php artisan make:model Post -mcr
+php artisan migrate
+php artisan make:migration change_posts_table
+php artisan migrate
+php artisan help migrate
+php artisan migrate
+php artisan migrate --step
+php artisan migrate --step -v
+php artisan migrate --step -vv
+php artisan migrate --step -vvv
+php artisan migrate:rollback
+php artisan -f migrate:rollback
+php artisan -force migrate:rollback
+php artisan migrate:rollback
+php artisan migrate:reset
+php artisan migrate:refresh
+php artisan migrate:refresh --seed
+php artisan migrate:fresh
+php artisan migrate 
+php artisan migrate:rollback
+php artisan migrate 
+php artisan migrate:refresh --step=2
+php artisan help migrate:refresh
+php artisan migrate:refresh --step=2
+php artisan migrate:refresh --step=2
+php artisan db:seed
+php artisan migrate:refresh --step=2
+php artisan db:seed
